5d5b938959ec0be7a5c45852dce010066c12a8ea
LimDae94 <piay801@naver.com>
2023-11-29 15:11:34 +0900

    #1-스프링 프로젝트 세팅하기
    ##  프로젝트 생성
    
    ### start.spring.io
    
    - **Project**: Gradle - Groovy
    - **Language**: Java
    - **Spring Boot**: **3.2.0**
    - **Project Metadata**:
      - Group: jpabook
      - Aritfact: jpashop
      - Name: jpashop
      - Description: Demo project for Spring Boot
      - Package name: jpabook.jpashop
      - Packaging: Jar
      - **Java: 17**
    - **Dependencies**:
      - Spring Web
      - Thymeleaf
      - Spring Data JPA
      - Validation
      - H2 Database
      - Lombok
      - Spring Boot Dev Tools
      - `implementation 'com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.0'`
    
    ### 스프링 생성
    start.spring.io

---
+++ jpastore/src/main/java/woottech/jpastore/JpastoreApplication.java
@@ -0,0 +1,13 @@
+package woottech.jpastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpastoreApplication.class, args);
+	}
+
+}
