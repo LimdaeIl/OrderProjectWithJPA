2efbd89aadf107c1cfcadf335904126b874a3d8d
LimDae94 <piay801@naver.com>
2023-11-29 17:43:05 +0900

    #4-회원 서비스 개발
    기술 설명
    `@Service`
    `@Transactional` : 트랜잭션, 영속성 컨텍스트
    `readOnly=true` : 데이터의 변경이 없는 읽기 전용 메서드에 사용, 영속성 컨텍스트를 플러시 하지 않
    으므로 약간의 성능 향상(읽기 전용에는 다 적용)
    데이터베이스 드라이버가 지원하면 DB에서 성능 향상
    `@Autowired`: 생성자 Injection 많이 사용, 생성자가 하나면 생략 가능
    
    기능 설명
    `join()`: 회원 가입
    `findMembers()`: 전체 회원 조회
    `findOne()`: 한 명 회원 조회

---
+++ jpastore/src/main/java/woottech/jpastore/service/MemberService.java
@@ -0,0 +1,48 @@
+package woottech.jpastore.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import woottech.jpastore.domain.Member;
+import woottech.jpastore.repository.MemberRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+}
+
