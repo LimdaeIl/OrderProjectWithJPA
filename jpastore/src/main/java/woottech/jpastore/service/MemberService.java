610a06d3a8782fa7724fb7ade1c129c9e5940a5b
LimDae94 <piay801@naver.com>
2023-11-29 19:17:10 +0900

    #4-에서 필드 주입에 사용된 @Autowired 삭제
    필드 주입에서 생성자 주입으로 변경되면서 임포트한 내용 삭제합니다.

--- jpastore/src/main/java/woottech/jpastore/service/MemberService.java
+++ jpastore/src/main/java/woottech/jpastore/service/MemberService.java
@@ -1,7 +1,6 @@
 package woottech.jpastore.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import woottech.jpastore.domain.Member;
