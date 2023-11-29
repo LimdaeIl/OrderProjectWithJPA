f0bf880f7937e41b0a01751214717a7f745d0db4
LimDae94 <piay801@naver.com>
2023-11-29 18:59:32 +0900

    #6-상품 엔티티 개발(비즈니스 로직 추가)
    ### 비즈니스 로직 분석
    
    `addStock()` 메서드는 파라미터로 넘어온 수만큼 재고를 늘립니다. 이 메서드는 재고가 증가하거나 상품 주문을 취소해서 재고를 다시 늘려야 할 때 사용합니다.
    `removeStock()` 메서드는 파라미터로 넘어온 수만큼 재고를 줄입니다. 만약 재고가 부족하면 예외가 발생합니다. 주로 상품을 주문할 때 사용합니다.

--- jpastore/src/main/java/woottech/jpastore/domain/item/Item.java
+++ jpastore/src/main/java/woottech/jpastore/domain/item/Item.java
@@ -3,6 +3,7 @@ package woottech.jpastore.domain.item;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import woottech.jpastore.exception.NotEnoughStockException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +13,7 @@ import java.util.List;
 @DiscriminatorColumn(name = "dtype")
 @Getter
 @Setter
-public class Item {
+public abstract class Item {
 
     @Id @GeneratedValue
     @Column(name = "item_id")
@@ -24,4 +25,18 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //== 비즈니스 로직 ==//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+
+        if (restStock < 0 ) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
