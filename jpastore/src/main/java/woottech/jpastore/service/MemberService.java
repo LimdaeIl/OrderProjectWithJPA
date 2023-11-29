74e20d1cfb85a0856d2b486383a7147c65b16355
LimDae94 <piay801@naver.com>
2023-11-29 17:55:00 +0900

    #4-회원 서비스 @RequiredArgsConstructor 로 생성자 주입
    lombok 라이브러리로 이전에 작성한 생성자 주입을 더 짧고 가독성이 뛰어나게 작성할 수 있습니다.
    
    기술 설명
    `@RequiredArgsConstructor`: 클래스에 선언된 final 변수들, 필드들을 매개변수로 하는 생성자를 자동으로 생성해주는 어노테이션입니다.

--- jpastore/src/main/java/woottech/jpastore/service/MemberService.java
+++ jpastore/src/main/java/woottech/jpastore/service/MemberService.java
@@ -1,5 +1,6 @@
 package woottech.jpastore.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,13 +11,10 @@ import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
-
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     /**
      * 회원가입
