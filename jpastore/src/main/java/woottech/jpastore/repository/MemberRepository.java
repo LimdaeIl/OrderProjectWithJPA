20bfe73e7aa8110ecbc9217e6b0cdd9a02ecc415
LimDae94 <piay801@naver.com>
2023-11-29 17:40:24 +0900

    #4-회원 리포지토리 개발
    ### 기술 설명
    `@Repository` : 스프링 빈으로 등록, JPA 예외를 스프링 기반 예외로 예외 변환
    `@PersistenceContext` : 엔티티 메니저( EntityManager ) 주입
    `@PersistenceUnit` : 엔티티 메니터 팩토리( EntityManagerFactory ) 주입
    
    ### 기능 설명
    기능 설명
    1. `save()`: 영속성 컨텍스트에 회원 저장
    2. `findOne()`: 한 명 회원 조회
    3. `findAll()`: 모든 회원을 조회
    4. `findByName()`: 회원 이름으로 조회

---
+++ jpastore/src/main/java/woottech/jpastore/repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package woottech.jpastore.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import woottech.jpastore.domain.Member;
+
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name =: name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
