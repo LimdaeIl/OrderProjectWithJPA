45a3076c4fdcaeef15fcbde454934c48cbc91557
LimDae94 <piay801@naver.com>
2023-11-29 18:40:58 +0900

    #4-회원 서비스 테스트 작성
    ### 테스트 케이스를 위한 설정
    테스트는 케이스 격리된 환경에서 실행하고, 끝나면 데이터를 초기화하는 것이 좋습니다. 그런 면에서 메모리 DB를 사용하는 것이 가장 이상적입니다.
    추가로 테스트 케이스를 위한 스프링 환경과, 일반적으로 애플리케이션을 실행하는 환경은 보통 다르므로 설정 파일을 다르게 사용합니다.
    
    다음과 같이 간단하게 테스트용 설정 파일을 추가하면 됩니다.
    test/resources/application.yml
    ```java
    ...
    logging.level:
      org.hibernate.SQL: debug
    # org.hibernate.type: trace
    ```
    
    이제 테스트에서 스프링을 실행하면 이 위치에 있는 설정 파일을 읽습니다. (만약 이 위치에 없으면 src/resources/application.yml 을 읽습니다.)
    스프링 부트는 datasource 설정이 없으면, 기본적을 메모리 DB를 사용하고, driver-class도 현재 등록된 라이브러리를 보고 찾아줍니다. 추가로 ddl-auto 도 create-drop 모드로 동작한다. 따라서 데이터소스나, JPA 관련된 별도의 추가 설정을 하지 않아도 됩니다.
    
    ### 기술 설명
    `@SpringBootTest` : 스프링 부트 띄우고 테스트(이게 없으면 @Autowired 다 실패)
    `@Transactional` : 반복 가능한 테스트 지원, 각각의 테스트를 실행할 때마다 트랜잭션을 시작하고 테스트
    가 끝나면 트랜잭션을 강제로 롤백 (이 어노테이션이 테스트 케이스에서 사용될 때만 롤백)
    
    ### 기능 설명
    회원가입 테스트
    중복 회원 예외처리 테스트

---
+++ jpastore/src/test/java/woottech/jpastore/service/MemberServiceTest.java
@@ -0,0 +1,52 @@
+package woottech.jpastore.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import woottech.jpastore.domain.Member;
+import woottech.jpastore.repository.MemberRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        // Given
+        Member member = new Member();
+        member.setName("kim");
+
+        // When
+        Long saveId = memberService.join(member);
+
+        // Then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // Given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // When
+        memberService.join(member1);
+        IllegalStateException thrown =
+                assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        // Then
+        assertEquals("이미 존재하는 회원입니다.", thrown.getMessage());
+    }
+}
