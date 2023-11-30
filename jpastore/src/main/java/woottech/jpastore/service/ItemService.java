d3881ed7bee1e055dd0750be65d0e00073d8312b
LimDae94 <piay801@naver.com>
2023-11-30 17:16:59 +0900

    #12-상품 서비스 업데이트 추가
    상품 업데이트를 추가합니다.

--- jpastore/src/main/java/woottech/jpastore/service/ItemService.java
+++ jpastore/src/main/java/woottech/jpastore/service/ItemService.java
@@ -15,6 +15,7 @@ public class ItemService {
 
     private final ItemRepository itemRepository;
 
+
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
@@ -26,5 +27,17 @@ public class ItemService {
 
     public Item findOne(Long itemId) {
         return itemRepository.findOne(itemId);
+    }
+
+    /**
+     * 영속성 컨텍스트가 자동 변경
+     */
+    @Transactional
+    public void updateItem(Long id, String name, int price, int stockQuantity)
+    {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
     }
 }
