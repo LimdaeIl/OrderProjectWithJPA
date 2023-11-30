3c407b6b0aca54dfd654891b2dec30050181d732
LimDae94 <piay801@naver.com>
2023-11-30 16:26:56 +0900

    #10-회원 등록 컨트롤러
    회원 등록 컨트롤러 구현

---
+++ jpastore/src/main/java/woottech/jpastore/web/MemberController.java
@@ -0,0 +1,37 @@
+package woottech.jpastore.web;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import woottech.jpastore.domain.Address;
+import woottech.jpastore.domain.Member;
+import woottech.jpastore.service.MemberService;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping(value = "/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
