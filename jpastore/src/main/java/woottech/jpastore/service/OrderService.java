931059b9416162736ebab97dd9538d951fa5d138
LimDae94 <piay801@naver.com>
2023-11-30 15:05:23 +0900

    #8-주문 서비스 개발
    주문 서비스는 주문 엔티티와 주문 상품 엔티티의 비즈니스 로직을 활용해서 주문, 주문 취소, 주문 내역 검색 기능을 제공합니다.
    
    참고: 예제를 단순화하려고 한 번에 하나의 상품만 주문할 수 있습니다.
    
    주문( `order()` ): 주문하는 회원 식별자, 상품 식별자, 주문 수량 정보를 받아서 실제 주문 엔티티를 생성한 후 저장합니다.
    
    주문 취소( `cancelOrder()` ): 주문 식별자를 받아서 주문 엔티티를 조회한 후 주문 엔티티에 주문 취소를 요청합니다.
    
    주문 검색( `findOrders()` ): OrderSearch 라는 검색 조건을 가진 객체로 주문 엔티티를 검색합니다. 자세한 내용은 다음에 나오는 주문 검색 기능에서 알아볼 수 있습니다.
    
    ----------------
    
    > 참고: 주문 서비스의 주문과 주문 취소 메서드를 보면 비즈니스 로직 대부분이 엔티티에 있습니다. 서비스 계층은 단순히 엔티티에 필요한 요청을 위임하는 역할을 합니다. 이처럼 엔티티가 비즈니스 로직을 가지고 객체 지향의 특성을 적극 활용하는 것을 도메인 모델 패턴(http://martinfowler.com/eaaCatalog/
    domainModel.html)이라 합니다.
    반대로 엔티티에는 비즈니스 로직이 거의 없고 서비스 계층에서 대부분의 비즈니스 로직을 처리하는 것을 트랜잭션 스크립트 패턴(http://martinfowler.com/eaaCatalog/
    transactionScript.html)이라 합니다.

---
+++ jpastore/src/main/java/woottech/jpastore/service/OrderService.java
@@ -0,0 +1,68 @@
+package woottech.jpastore.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import woottech.jpastore.domain.*;
+import woottech.jpastore.domain.item.Item;
+import woottech.jpastore.repository.ItemRepository;
+import woottech.jpastore.repository.MemberRepository;
+import woottech.jpastore.repository.OrderRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = ItemRepository.findOne(itemId);
+
+        // 배송 정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        // 주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+
+        // 주문 취소
+        order.cancel();
+    }
+
+    /**
+     * 주문 검색
+     */
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAll(orderSearch);
+    }
+
+}
