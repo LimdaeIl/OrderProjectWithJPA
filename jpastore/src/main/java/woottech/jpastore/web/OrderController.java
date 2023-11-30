66306ec16c28d111d99faea86b405f95529ac674
LimDae94 <piay801@naver.com>
2023-11-30 17:14:31 +0900

    #12- 주문 컨트롤러 추가
    ### 주문 폼 이동
    메인 화면에서 상품 주문을 선택하면 /order 를 GET 방식으로 호출
    OrderController 의 createForm() 메서드
    주문 화면에는 주문할 고객정보와 상품 정보가 필요하므로 model 객체에 담아서 뷰에 넘겨줌
    
    ### 주문 실행
    주문할 회원과 상품 그리고 수량을 선택해서 Submit 버튼을 누르면 /order URL을 POST 방식으로 호출
    컨트롤러의 order() 메서드를 실행
    이 메서드는 고객 식별자( memberId ), 주문할 상품 식별자( itemId ), 수량( count ) 정보를 받아서 주문 서비스에 주문을 요청
    주문이 끝나면 상품 주문 내역이 있는 /orders URL로 리다이렉트

---
+++ jpastore/src/main/java/woottech/jpastore/web/OrderController.java
@@ -0,0 +1,56 @@
+package woottech.jpastore.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import woottech.jpastore.domain.Member;
+import woottech.jpastore.domain.Order;
+import woottech.jpastore.domain.OrderSearch;
+import woottech.jpastore.domain.item.Item;
+import woottech.jpastore.service.ItemService;
+import woottech.jpastore.service.MemberService;
+import woottech.jpastore.service.OrderService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping(value = "/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+        return "order/orderForm";
+    }
+
+    @PostMapping(value = "/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId, @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping(value = "/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch
+                                    orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "order/orderList";
+    }
+
+    @PostMapping(value = "/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
+    }
+
+}
