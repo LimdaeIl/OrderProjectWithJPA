9dd646b40d6f922673826fc34defee877978ee44
LimDae94 <piay801@naver.com>
2023-11-30 17:15:50 +0900

    #12-상품 수정, 권장 코드로 변경
    상품 수정, 권장 코드로 변경

--- jpastore/src/main/java/woottech/jpastore/repository/ItemRepository.java
+++ jpastore/src/main/java/woottech/jpastore/repository/ItemRepository.java
@@ -3,7 +3,12 @@ package woottech.jpastore.repository;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import woottech.jpastore.domain.item.Item;
+import woottech.jpastore.service.ItemService;
+import woottech.jpastore.web.BookForm;
 
 import java.util.List;
 
@@ -12,6 +17,7 @@ import java.util.List;
 public class ItemRepository {
 
     private final EntityManager em;
+    private final ItemService itemService;
 
     public void save(Item item) {
         if (item.getId() == null) {
@@ -23,7 +29,20 @@ public class ItemRepository {
     public Item findOne(Long id) {
         return em.find(Item.class, id);
     }
+
     public List<Item> findAll() {
         return em.createQuery("select i from Item i",Item.class).getResultList();
     }
+
+    /**
+     * 상품 수정, 권장 코드
+     */
+    @PostMapping(value = "/items/{itemId}/edit")
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form")
+    BookForm form) {
+        itemService.updateItem(itemId, form.getName(), form.getPrice(),
+                form.getStockQuantity());
+        return "redirect:/items";
+    }
+
 }
